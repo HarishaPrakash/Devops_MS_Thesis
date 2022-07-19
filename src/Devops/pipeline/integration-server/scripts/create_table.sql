7a84532b83488c0661ca27cf76c41cdfb54a8cd9
Harisha Prakash <79756878+HarishaPrakash@users.noreply.github.com>
2022-07-19 18:58:45 +0200

    Update create_table.sql

--- src/Devops/pipeline/integration-server/scripts/create_table.sql
+++ src/Devops/pipeline/integration-server/scripts/create_table.sql
@@ -8,10 +8,10 @@ CREATE TABLE  IF NOT EXISTS metrics(commit_id varchar(100),
                             nloc_deleted integer(100),
                             change_rate DOUBLE,
                             cyclomatic_complexity integer(100),
-                            effective_cylomatic_complexity DOUBLE,
+                            effective_cyclomatic_complexity DOUBLE,
                             unit_test_case integer(100),
                             failed_unit_test_case integer(100),
-                            integration_test_case integer(100),
-                            failed_integration_test_case integer(100),
+                            acceptance_test_case integer(100),
+                            failed_acceptance_test_case integer(100),
                             production_deployment datetime
                             );
